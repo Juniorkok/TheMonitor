bc66371b0bc3ac0e14f9aa18025109842c673fac
Yann Dias <yanndias@live.fr>
2019-07-17 18:14:50 +0200

    Ajout bootstrap, categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :categories
   get 'home/index'
   
   resources :articles do
     resources :comments
   end
 
+  get '/admin/articles' => 'articles#index_admin'
   root 'home#index'
 end
